22a435fc249bf67fccab00a17af52b45bdfc3af1
Nit89 <nitesh382singh.com>
2024-09-26 01:22:11 +0530

    gcp9

--- main.tf
+++ main.tf
@@ -1,27 +1,12 @@
-# resource "google_storage_bucket" "my-bucket" {
-#   name          = "tt-github-nit-9099"
-#   location      = "US"
-#   force_destroy = true
-#   project = "eco-hangar-381618"
-#   public_access_prevention = "enforced"
-# }
-
-resource "random_id" "default" {
-  byte_length = 8
+resource "google_storage_bucket" "my-bucket" {
+  name          = "tt-github-nit-9099"
+  location      = "US"
+  force_destroy = true
+  project = "eco-hangar-381618"
+  public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "default" {
-  name     = "${random_id.default.hex}-terraform-remote-backend"
-  location = "US"
- project = "eco-hangar-381618"
-  force_destroy               = false
-  public_access_prevention    = "enforced"
-  uniform_bucket_level_access = true
 
-  versioning {
-    enabled = true
-  }
-}
 
 # from flask import Flask
 
